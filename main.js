7be9937a35b2d9b555d14c35049792200d05bd32
Ankush <ankushsharma9685@gmail.com>
2023-09-23 18:35:11 +0530

    Added javascript to perform dom manuplation

---
+++ main.js
@@ -0,0 +1,36 @@
+
+let formData = document.getElementById("my-form");
+//listen for event
+formData.addEventListener("submit", (e) => {
+    //so not lose state
+    e.preventDefault();
+    let ans = {};
+    //because data is form data
+    let data = new FormData(e.target);
+    //destructure name and value
+    for (let [name, value] of data) {
+        if (!value) {
+            alert(`${name} is empty`);
+            return;
+        }
+        ans[name] = value;
+    }
+    //printing result
+    console.log(ans);
+});
+
+let btn = document.querySelector(".btn");
+btn.addEventListener('click', e => {
+    console.log(e);
+    e.target.value = "Open the console.log ==>";
+});
+
+btn.addEventListener('mouseover', e => {
+    console.log("hello")
+    e.target.value = "Click Me"
+})
+
+btn.addEventListener('mouseout', e => {
+    console.log("hello")
+    e.target.value = "Submit"
+})
