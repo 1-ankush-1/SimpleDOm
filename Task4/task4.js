99c0604b191c09ebef909dd348868e9aea61064c
Ankush <ankushsharma9685@gmail.com>
2023-09-25 17:01:04 +0530

    Added delete a user function

--- Task4/task4.js
+++ Task4/task4.js
@@ -13,8 +13,6 @@ function addUser(e) {
 
 //add user in table
 function addinTable(user) {
-    //get body
-    let body = document.getElementById("tablebody");
     //create row element
     let row = document.createElement('tr');
     //create three table data element and put user detail
@@ -24,14 +22,42 @@ function addinTable(user) {
     email.textContent = user.email;
     let phone = document.createElement('td');
     phone.textContent = user.phone;
+    let operations = document.createElement('td');
+    let btndel = document.createElement('button');
+    btndel.className = "btn btn-danger delete";
+    btndel.id = "del";
+    btndel.textContent = "Delete";
+    operations.appendChild(btndel);
     //three child in row
     row.appendChild(name);
     row.appendChild(email);
     row.appendChild(phone);
+    row.appendChild(operations);
     //add row in body
     body.appendChild(row)
+}
+
+function deleteUser(e) {
+    e.preventDefault();
+    //check if any class contains delete
+    if (e.target.classList.contains('delete')) {
+        if (confirm("Are you sure!")) {
+            //get the row
+            let row = e.target.parentElement.parentElement;
+            //remove the row from table
+            body.removeChild(row);
+            //remove the row from localhost
+            localStorage.removeItem(row.cells[1].innerText);
+        }
+    }
+
 }
 
 //get form data and listen for event
 let formData = document.getElementById("demoForm");
 formData.addEventListener('submit', addUser);
+
+//get tablebody to delete user
+let body = document.getElementById("tablebody");
+body.addEventListener('click', deleteUser);
+
