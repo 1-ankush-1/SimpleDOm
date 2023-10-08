ebaf65435abedda863345893ce1944a40e630fd8
Ankush <ankushsharma9685@gmail.com>
2023-10-08 19:18:57 +0530

    added routes to home node and about and a 404 page

--- NodeStart/server.js
+++ NodeStart/server.js
@@ -2,10 +2,19 @@ const http = require("http");
 
 //creating an server
 const app = http.createServer((req, res) => {
+    const url = req.url;
     res.setHeader('Content-Type', 'text/html');
     res.write("<html>")
-    res.write("<head><title>My First Page</title></head>")
-    res.write("<body><h1>Welcome to My Nodejs Project</h1></body>");
+    res.write("<head><title>Routes</title></head>")
+    if (url === "/home") {
+        res.write("<body><h1>Welcome Home</h1></body>");
+    } else if (url === "/about") {
+        res.write("<body><h1>Welcome to About us Page</h1></body>");
+    } else if (url === "/node") {
+        res.write("<body><h1>Welcome to My Nodejs Project</h1></body>");
+    } else {
+        res.write("<body><h1>404 No Page Found</h1></body>");
+    }
     res.write("</html>")
     res.end();
 });
