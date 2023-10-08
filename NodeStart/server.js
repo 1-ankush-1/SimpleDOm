4b3dc911a60725c38923f0b5288cdbde0ea1ef64
Ankush <ankushsharma9685@gmail.com>
2023-10-08 19:04:30 +0530

    added a html h1 response to the root request

--- NodeStart/server.js
+++ NodeStart/server.js
@@ -2,7 +2,11 @@ const http = require("http");
 
 //creating an server
 const app = http.createServer((req, res) => {
-    res.write("Ankush");
+    res.setHeader('Content-Type', 'text/html');
+    res.write("<html>")
+    res.write("<head><title>My First Page</title></head>")
+    res.write("<body><h1>Welcome to My Nodejs Project</h1></body>");
+    res.write("</html>")
     res.end();
 });
 
