ce70e51ca0b33ecee25454f60a5adc29634af05d
Ankush <ankushsharma9685@gmail.com>
2023-10-10 14:28:49 +0530

    added body parser and add-product route

--- StartedWithExpress/server.js
+++ StartedWithExpress/server.js
@@ -1,16 +1,43 @@
+const bodyParser = require("body-parser");
 const express = require("express");
 
-//initalise new objectd
+//initalise new exprees object
 const app = express();
 
-//use - it allow to use middleware
-app.use((req, res, next) => {
-    console.log("in middleware");
-    next(); //pass the request to next middleware
+//body-parser do the req body parsing(handel form body not files and json)
+app.use(
+    bodyParser.urlencoded({
+      extended: true,
+    }),
+  );
+
+//add product route
+app.use("/add-product", (req, res, next) => {
+    res.send(`
+    <html>
+        <head>
+            <title>Product</title>
+        </head>
+        <body>
+            <form action="/product" method="POST">
+                <input type="text" name="name">
+                <input type="number" name="size">
+                <button type="submit">Submit</button>
+            </form>
+        </body>
+    </html>
+    `);
 })
 
+//handel post request and redirect to home
+app.post("/product", (req, res, next) => {
+    console.log(req.body)
+    res.redirect("/");
+})
+
+//root route
 app.use("/", (req, res, next) => {
-    res.send({ "one": "three" });  //send response of anykind
+    res.send("HomePage");  //send response of anykind
 })
 
 //start the server
