11e4ed870bd8e909e97514aa80d46c7268fced4a
Ankush <ankushsharma9685@gmail.com>
2023-10-12 16:46:27 +0530

    added controller and added the contact ejs and 404.ejs

--- StartedWithExpress/Routes/admin.js
+++ StartedWithExpress/Routes/admin.js
@@ -1,17 +1,9 @@
-const path = require("path");
-const express = require("express");
-const rootDir = require("../util/path.js");
+const express = require('express');
+const productController = require('../controllers/productController.js');
 const router = express.Router();
 
-//add product route
-router.get("/add-product", (req, res, next) => {
-    res.sendFile(path.join(rootDir, "views", "add-product.html"))
-})
+router.get('/add-product', productController.getAddProduct);
 
-//handel post request and redirect to home
-router.post("/add-product", (req, res, next) => {
-    console.log(req.body)
-    res.redirect("/shop");
-})
+router.post('/add-product', productController.postAddProduct);
 
-module.exports = router;
+module.exports = router
