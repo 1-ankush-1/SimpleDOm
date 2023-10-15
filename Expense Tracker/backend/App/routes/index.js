fb94d256b2e412067e0217e44aefcd7a238a29ae
Ankush <ankushsharma9685@gmail.com>
2023-10-15 18:30:08 +0530

    Made basic setup and added routes

---
+++ Expense Tracker/backend/App/routes/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+
+router.use("/expense", (req, res, next) => {
+    console.log("in");
+});
+
+//if no route found
+router.use((req, res, next) => {
+    res.status(404).send("no routes found");
+})
+
+module.exports = router;
